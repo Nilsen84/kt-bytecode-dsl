4f0b03bf89ae1e5fdca6cc8dc3a5964294e544d6
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-07-08 22:09:05 +0200

    Maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     `java-library`
+    `maven-publish`
 }
+
+group = "com.github.nilsen84"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -9,4 +13,12 @@ repositories {
 
 dependencies {
     implementation("org.ow2.asm:asm-tree:9.4")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
 }
